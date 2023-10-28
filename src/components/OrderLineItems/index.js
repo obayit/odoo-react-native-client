ff13bcbbe43aee8a635cd5107dcafa6f6e756867
Ubay Abdelgadir <obayit@gmail.com>
2023-10-28 19:52:30 +0200

    minor changes

--- src/components/OrderLineItems/index.js
+++ src/components/OrderLineItems/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
-import { Text, useStyleSheet } from '@ui-kitten/components';
+import { Text, useStyleSheet, StyleService } from '@ui-kitten/components';
 
 import { ReusableStyles } from '..';
 
@@ -13,7 +13,7 @@ const HorizontalItem = ({children}) => {
 }
 
 export default ({ recordIds, orderId }) => {
-    const { useQuery } = injectQuery('sale.order.line', { 
+    const { useQuery } = injectQuery('sale.order.line', {
         fields: [
             'id',
             'product_id',
@@ -24,16 +24,29 @@ export default ({ recordIds, orderId }) => {
     });
     const { data, isLoading, refetch } = useQuery();
 
+    const styles = useStyleSheet(themedStyles)
     const rs = useStyleSheet(ReusableStyles)
     if(data?.records?.length){
         return (
-            <ScrollView horizontal={true} style={{margin: 5, marginTop: 0}}>
+            <ScrollView horizontal={true} style={styles.container}>
                 {data?.records?.map((line_id, index) =><HorizontalItem>
-                    <Text key={line_id.id}>{data.records.length}:{displayM2o(line_id.product_id)}:{line_id.product_uom_qty}</Text>
+                    <Text key={line_id.id}>{displayM2o(line_id.product_id)}:{line_id.product_uom_qty}</Text>
                 </HorizontalItem>)}
             </ScrollView>
         );
     }else{
-        return <View style={{height: 40}}/>
+        return <View style={styles.placeholder}/>
     }
 };
+
+const themedStyles = StyleService.create({
+  container: {
+    margin: 5,
+    marginTop: 0,
+    borderRadius: 5,
+    backgroundColor: 'color-info-100',
+  },
+  placeholder: {
+    height: 40
+  }
+})
